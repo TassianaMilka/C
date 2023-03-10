a569cf940beb5e155c10d39e89f61605d12c8e09
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2023-03-09 21:04:03 -0300

    4

--- Simple Programs -C/LetraNu.c
+++ Simple Programs -C/LetraNu.c
@@ -1,12 +1,8 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
 
-*******************************************************************************/
-
-#include <stdio.h>
+#include <stdio.h> //Bliotecas
+#include<stdlib.h>
+#include<locale.h>
 
 //Pela Tabela de Letra tendo as suas representaçõas cada letra e imprimir no final a Frase
 //Realizando na Linguagem c e Compilando o Resultado.
