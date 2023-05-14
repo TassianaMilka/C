03fe1d55fe134a590dedb6b2b2bf21185e12922c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2023-05-14 09:13:54 -0300

    Update Imprimir1.C#.cs
    1

--- Simple Programs -C#/Imprimir1.C#.cs
+++ Simple Programs -C#/Imprimir1.C#.cs
@@ -9,7 +9,7 @@ class Imprimir{//Nome da Classe
  public static void Main(){//Sem o Main deixando só a classe com seu nome não vai compilar.
     //Imprimir o texto. 
     Console.WriteLine("                  Aprender                           ");  
-    Console.WriteLine("Começando a Programar em C# na função de aprender mais a programar\ntodos os códigos serão colocados no repositório do Github.");
+    Console.WriteLine("Começando a programar em C# na função de aprender mais a programar\ntodos os códigos serão colocados no repositório do Github.");
     Console.WriteLine("Estudar para mim é algo incrível no meu aprendizagem.");
     Console.WriteLine("Afinal no passar do tempo aprende o  que é realmente inteligência\nno desenvolvimento da vida.Simplemente não é  dizer isso aquilo,mas sim o profundo.");
   
