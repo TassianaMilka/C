ec26cf539888d24b6928800445d88451615d0251
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-12 21:41:08 -0300

    2

---
+++ C/Simple Programs/ZeroMeMa1.c
@@ -0,0 +1,45 @@
+
+//Bibliotecas
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//Crie um programa que tenha entrada de dado,depois que possa fazer o 1000 de maneira menor e maior do retorno 
+//para imprimir e visualizar vários zeros(0).
+
+int main()
+{ 
+    
+    //Declarações da variáveis.
+    
+    float numeroP;
+    float numero;
+    
+    
+    printf("Informe o numero");//Entrada de dado 
+    scanf("%f",&numeroP);      //Saída de dado  
+    
+
+    //Operando uma realização lógica do menor e maior.
+    if(numero<=1000){   
+         
+
+    }else if(numero>=1000){
+        
+        
+    }
+     
+  
+      
+     //Imprimir o resultado final igual a vários zeros.
+     
+      printf("O resultado obtido a zero\n%f",numero); 
+
+
+   
+    return 0;
+} 
+ 
+ 
