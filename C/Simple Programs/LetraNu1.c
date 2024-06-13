c4de26170ce8ef7df8c9654bfa9e898e6ee17b4a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-12 22:01:31 -0300

    4

---
+++ C/Simple Programs/LetraNu1.c
@@ -0,0 +1,393 @@
+
+//Bibliotecas
+#include <stdio.h> 
+#include<stdlib.h>
+#include<locale.h>
+
+
+//setlocale(LC_ALL, "portuguese");
+
+//Pela Tabela de Letra tendo as suas representaçõas cada letra e imprimir no final a Frase
+//Realizando na Linguagem c e Compilando o Resultado.
+
+
+
+int main()
+{
+
+   //Declaração da Variável.
+   int LetraNu1;
+   int LetraNu2;
+   int LetraNu3;
+   int LetraNu4;
+   int LetraNu5;
+   int LetraNu6;
+   int LetraNu7;
+   int LetraNu8;
+   int LetraNu9;
+   int LetraNu10;
+   int LetraNu11;
+   int LetraNu12;
+   int LetraNu13; 
+   int LetraNu14;
+   int LetraNu15;
+   int LetraNu16;
+   int LetraNu17;
+   int LetraNu18;
+   int LetraNu19;
+   int LetraNu20;
+   int LetraNu21;
+   int LetraNu22;
+   int LetraNu23;
+   int LetraNu24;
+   int LetraNu25;
+   int LetraNu26;
+   int LetraNu27;
+   int LetraNu28;
+   int LetraNu29;
+   int LetraNu30;
+   int LetraNu31;
+   int LetraNu32;
+   int LetraNu33;
+   int LetraNu34;
+   int LetraNu35;
+   int LetraNu36;
+   int LetraNu37;
+   int LetraNu38;
+   int LetraNu39;
+   int LetraNu40;
+   int LetraNu41;
+   int LetraNu42;
+   int LetraNu43;
+                                
+   
+  
+
+    //Tabela.
+    printf("A-Representa-6\n");
+    printf("B-Representa-8\n");
+    printf("C-Representa-2\n");
+    printf("D-Reprsenta-5\n");
+    printf("E-Representa-3\n");
+    printf("F-Representa-1\n");
+    printf("G-Representa-0\n");
+    printf("H-Representa-4\n");
+    printf("I-Reprsenta-11\n");
+    printf("J-Representa-9\n");
+    printf("K-Reprsenta-12\n");
+    printf("L-Representa-18\n");
+    printf("M-Representa-22\n");
+    printf("N-Representa-14\n");
+    printf("O-Representa-20\n");
+    printf("P-Representa-13\n");
+    printf("Q-Representa-19\n");
+    printf("R-Representa-15\n");
+    printf("S-Representa-23\n");
+    printf("T-Representa-17\n");
+    printf("U-Representa-26\n");
+    printf("V-Representa-25\n");
+    printf("W-Representa-16\n");
+    printf("X-Representa-47\n");
+    printf("Y-Representa-18\n");
+    printf("Z-Representa-24\n");
+    printf("ã-Representa-27\n");
+    printf("ç-Representa-34\n");
+    printf(",-Representa-42\n");
+    printf(".-Representa-40\n");
+    printf("^-Representa-31\n");
+    printf("[-Representa-33\n");
+    printf("]-Representa-32\n");
+    printf("*-Representa-35\n");
+    printf("&-Representa-38\n");
+    printf("$-Representa-36\n");
+    printf("@-Representa-39\n");
+    printf("!-Representa-37\n");
+    printf("'-Representa-42\n");
+    printf("<-Representa-34\n");
+    printf(">-Representa-38\n");
+    printf("\-Representa-44\n");
+    printf("|-Representa-39\n");
+    printf("+-Representa-46\n");
+    printf("/-Representa-30\n");
+    printf("¨-Representa-43\n");
+    printf("?-Representa-41\n");
+    printf("=-Representa-45\n");
+    printf("(-Representa-48\n");
+    printf(")-Representa-49\n");
+    
+    
+   
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu1);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu2);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu3);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu4);//Saída de dado.
+    printf("\n\n");
+    
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu5);//Saída de dado.
+    printf("\n\n");
+    
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu6);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu7);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu8);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu9);//Saída de dado.
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu10);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu11);//Saída de dado.
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu12);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu13);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu14);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu15);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu16);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu17);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu18);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu19);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu20);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu21);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu22);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu23);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu24);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu25);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu26);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu27);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu28);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu29);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu30);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu31);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu32);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu33);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu34);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu35);//Saída de dado.
+    printf("\n\n");
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu36);//Saída de dado.
+    printf("\n\n"); 
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu37);//Saída de dado.
+    printf("\n\n"); 
+    
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu38);//Saída de dado.
+    printf("\n\n"); 
+    
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu39);//Saída de dado.
+    printf("\n\n"); 
+   
+   
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu40);//Saída de dado.
+    printf("\n\n"); 
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu41);//Saída de dado.
+    printf("\n\n"); 
+    
+    
+    
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu42);//Saída de dado.
+    printf("\n\n"); 
+
+    printf("Informe as letras e suas representações.\n");//Entrada de dado.
+    scanf("%d",&LetraNu43);//Saída de dado.
+    printf("\n\n"); 
+
+
+    //Imprimir o resultado.
+    printf("Realizando na Linguagem C e Compilando o resultado\n");
+    printf("Frase em Letra para numero:R\n%d",LetraNu1);
+    printf("\n"); 
+    printf("Frase em Letra para numero:e\n%d",LetraNu2);
+    printf("\n"); 
+    printf("Frase em Letra para numero:l\n%d",LetraNu3);
+    printf("\n");
+    printf("Frase em Letra para numero:i\n%d",LetraNu4);
+    printf("\n");
+    printf("Frase em Letra para numero:z\n%d",LetraNu5);
+    printf("\n");
+    printf("Frase em Letra para numero:a\n%d",LetraNu6);
+    printf("\n");
+    printf("Frase em Letra para numero:n\n%d",LetraNu7);
+    printf("\n");
+    printf("Frase em Letra para numero:d\n%d",LetraNu8);
+    printf("\n");
+    printf("Frase em Letra para numero:o\n%d",LetraNu9);
+    printf("\n");
+    printf("Frase em Letra para numero:n\n%d",LetraNu10);
+    printf("\n");
+    printf("Frase em Letra para numero:a\n%d",LetraNu11);
+    printf("\n");
+    printf("Frase em Letra para numero:L\n%d",LetraNu12);
+    printf("\n");
+    printf("Frase em Letra para numero:i\n%d",LetraNu13);
+    printf("\n");
+    printf("Frase em Letra para numero:n\n%d",LetraNu14);
+    printf("\n");
+    printf("Frase em Letra para numero:g\n%d",LetraNu15);
+    printf("\n");
+    printf("Frase em Letra para numero:u\n%d",LetraNu16);
+    printf("\n");
+    printf("Frase em Letra para numero:a\n%d",LetraNu17);
+    printf("\n");
+    printf("Frase em Letra para numero:g\n%d",LetraNu18);
+    printf("\n");
+    printf("Frase em Letra para numero:e\n%d",LetraNu19);
+    printf("\n");
+    printf("Frase em Letra para numero:m\n%d",LetraNu20);
+    printf("\n");
+    printf("Frase em Letra para numero:C\n%d",LetraNu21);
+    printf("\n");
+    printf("Frase em Letra para numero:e\n%d",LetraNu22);
+    printf("\n");
+    printf("Frase em Letra para numero:C\n%d",LetraNu23);
+    printf("\n");
+    printf("Frase em Letra para numero:o\n%d",LetraNu24);
+    printf("\n");
+    printf("Frase em Letra para numero:m\n%d",LetraNu25);
+    printf("\n");
+    printf("Frase em Letra para numero:p\n%d",LetraNu26);
+    printf("\n");
+    printf("Frase em Letra para numero:i\n%d",LetraNu27);
+    printf("\n");
+    printf("Frase em Letra para numero:l\n%d",LetraNu28);
+    printf("\n");
+    printf("Frase em Letra para numero:a\n%d",LetraNu29);
+    printf("\n");
+    printf("Frase em Letra para numero:n\n%d",LetraNu30);
+    printf("\n");
+    printf("Frase em Letra para numero:d\n%d",LetraNu31);
+    printf("\n");
+    printf("Frase em Letra para numero:o\n%d",LetraNu32);
+    printf("\n");
+    printf("Frase em Letra para numero:o\n%d",LetraNu33);
+    printf("\n");
+    printf("Frase em Letra para numero:R\n%d",LetraNu34);
+    printf("\n");
+    printf("Frase em Letra para numero:e\n%d",LetraNu35);
+    printf("\n");
+    printf("Frase em Letra para numero:s\n%d",LetraNu36);
+    printf("\n");
+    printf("Frase em Letra para numero:u\n%d",LetraNu37);
+    printf("\n");
+    printf("Frase em Letra para numero:l\n%d",LetraNu38);
+    printf("\n");
+    printf("Frase em Letra para numero:t\n%d",LetraNu39);
+    printf("\n");
+    printf("Frase em Letra para numero:a\n%d",LetraNu40);
+    printf("\n");
+    printf("Frase em Letra para numero:d\n%d",LetraNu41);
+    printf("\n");
+    printf("Frase em Letra para numero:o\n%d",LetraNu42);
+    printf("\n");
+    printf("Frase em Letra para numero:.\n%d",LetraNu43);
+
+ 
+    
+    
+
+
+    return 0;
+}
